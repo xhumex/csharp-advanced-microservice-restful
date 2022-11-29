42d6b92ac843f8ab1a3deb7a41a9c966de98fec0
cristian-hume <119268222+cristian-hume@users.noreply.github.com>
2022-11-28 21:13:41 -0300

    [feature/CREATE-PROJECT]: Initial Configuration Project
    * Add initial files and initial configurations for run project.
    
    Signed-off-by: Cristian Hume Henriquez <cristian.hume@programmer.net>

---
+++ CSharp.Advanced.Microservice.Restful/Program.cs
@@ -0,0 +1,24 @@
+﻿/**
+ *  Program.cs
+ * 
+ *  author   Cristian Hume Henriquez
+ *  version  1.0.0
+ *  created  2022-11-28
+ *  modified 2022-11-28
+ */
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
